7128681751fb297fcf0c8291169e64a802700bd4
DJSCE Student <pracs101@gmail.com>
2018-10-19 14:32:36 +0530

    Logout Spinner

--- src/Components/Logout/AdminLogout.js
+++ src/Components/Logout/AdminLogout.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
 import withErrorHandler from '../../HOC/withErrorHandler/withErrorHandler';
 import axios from '../../Axios';
 import RemoveItem from '../../HOC/LocalStorage/RemoveItem';
+import Spinner from '../../UI/Spinner/Spinner';
 
-const logout = (props) => {
-  let token = localStorage.getItem('token');
-  const headers = {
-    'x-auth': token
-  }
-  axios.delete('/admin/me/token', { headers })
-    .then(res => {
-      RemoveItem('token');
-      RemoveItem('id');
-      props.logout();
-    })
-    .catch(e => console.log(e));
-  return <Redirect to='/admin' />;
+class Logout extends Component {
+    state = {
+        loading: false
+    }
+    componentWillMount () {
+        let token = localStorage.getItem('token');
+        this.setState({loading: true});
+          const headers = {
+            'x-auth': token
+          }
+          axios.delete('/admin/me/token', { headers })
+            .then(res => {
+              RemoveItem('token');
+              RemoveItem('id');
+              this.props.logout();
+              this.setState({loading: false});
+        })
+        .catch(e => {
+              console.log(e)
+            this.setState({loading: false});
+          });
+    }
+    render () {
+        let ren = null;
+        if (this.state.loading) {
+            ren = <Spinner />;
+        }
+        else {
+            ren = <Redirect to='/admin' />;
+        }
+        return (
+            <div>
+                {ren}
+            </div>
+        );
+    }
 }
 
-export default withErrorHandler(logout, axios);
+export default withErrorHandler(Logout, axios);
--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
 import withErrorHandler from '../../HOC/withErrorHandler/withErrorHandler';
 import axios from '../../Axios';
 import RemoveItem from '../../HOC/LocalStorage/RemoveItem';
+import Spinner from '../../UI/Spinner/Spinner';
 
-const logout = (props) => {
-  let token = localStorage.getItem('token');
-  const headers = {
-    'x-auth': token
-  }
-  axios.delete('/user/me/token', { headers })
-    .then(res => {
-      RemoveItem('token');
-      RemoveItem('id');
-      RemoveItem('dept');
-      RemoveItem('name');
-      RemoveItem('type');
-      props.logout();
-    })
-    .catch(e => console.log(e));
-  return <Redirect to='/' />;
+class Logout extends Component {
+    state = {
+        loading: false
+    }
+    componentWillMount () {
+         let token = localStorage.getItem('token');
+        this.setState({loading: true});
+          const headers = {
+            'x-auth': token
+          }
+          axios.delete('/user/me/token', { headers })
+            .then(res => {
+              RemoveItem('token');
+              RemoveItem('id');
+              RemoveItem('dept');
+              RemoveItem('name');
+              RemoveItem('type');
+              this.props.logout();
+              this.setState({loading: false});
+            })
+            .catch(e => {
+              console.log(e)
+              this.setState({loading: false});
+          })
+    }
+    render () {
+        let ren = null;
+        if (this.state.loading) {
+            ren = <Spinner />;
+        }
+        else {
+            ren = <Redirect to='/' />;
+        }
+        return (
+            <div>
+                {ren}
+            </div>
+        )
+    }
 }
 
-export default withErrorHandler(logout, axios);
+export default withErrorHandler(Logout, axios);
